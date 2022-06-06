53d8e51ccc324ce8113a642da637fb11131dd41a
purnakarki65652 <purnakarki65652@gmail.com>
2022-06-06 18:21:04 +0545

    aja ko class 2079-02-22

--- inter the firest number.py
+++ inter the firest number.py
@@ -1,11 +1,17 @@
 from tkinter import *
 window = Tk()#object and function
 window.title("welcome to nepal airlines")#title text and content
-lbl = Label(window, text="Enter the First number",font=("Arial Bold",20))#Text and font
+
+lbl = Label(window, text="Enter First number",font=("Arial Bold",10))#Text and font
 lbl.grid(column=0,row=0)#lable column and row size
-lbl1 = Label(window,text="Enter the second number",font=("Arial Bold",20))
+
+txt = Entry(window,width=5)
+txt.grid(column=0,row=1)
+
+lbl1 = Label(window,text="Enter second number",font=("Arial Bold",10))
 lbl1.grid(column=0,row=2)
-txt = Entry(window,width=10,)
-txt.grid(column=1,row=1)
+
+txt1 = Entry(window,width=5)
+txt1.grid(column=0,row=3)
 
 window.mainloop()
