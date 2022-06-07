03a6ff17a71a8f3f57ff2d2135cd3b40f137d9fb
purnakarki65652 <purnakarki65652@gmail.com>
2022-06-07 18:27:13 +0545

    2079-02-24

---
+++ Manu Dashboard.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from tkinter import Menu
+
+from tkinter import  messagebox #import message called
+def openNewWindow():
+    #Toplevel object which will be treated as a new window
+    NewWindow = Toplevel(window)
+#sets the title of the toplovel widget
+    NewWindow.title("new window")
+#sets the geomtry of toplovel
+    NewWindow.geometry("200x200")
+# A label widget to show in toplevel
+    Label(NewWindow, text="This is a new window").pack()
+
+def save(): #function
+    messagebox.showinfo("Saved")
+
+window = Tk()
+window.title("Welcome to Dashboard")
+
+menu = Menu(window)
+new_item = Menu(menu, tearoff=0)# tearoff remove the ---- line
+new_item.add_command(label='New' , command=openNewWindow)
+new_item.add_separator()
+new_item.add_command(label='Print')
+new_item.add_separator()
+new_item.add_command(label='copy')
+new_item.add_command(label='paste')
+new_item.add_separator()
+new_item.add_command(label='Save',command=save) #save commadn
+new_item.add_command(label='Save as')
+
+menu.add_cascade(label='File',menu=new_item)
+window.config(menu=menu)
+
+window.mainloop()
--- inter the firest number.py
+++ inter the firest number.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 window = Tk()#object and function
 window.title("welcome to nepal airlines")#title text and content
 
@@ -13,5 +14,8 @@ lbl1.grid(column=0,row=2)
 
 txt1 = Entry(window,width=5)
 txt1.grid(column=0,row=3)
+
+but = Button(window, text="Calculator",bg="red")
+but.grid(column=2,row=5)
 
 window.mainloop()
